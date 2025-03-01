1723f7ee131b264a19cfe36537c0a932d1b04195
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-03-01 17:50:00 +0530

    My final Commit

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PageNotFound() {
   return (
     <div className='flex w-full justify-center'>
-      <span className='text-6xl font-bold leading-4 text-white'>404 Not Found</span>
+      <span className='text-6xl font-bold text-center text-white'>404 Not Found</span>
     </div>
   )
 }
